c76cafb0888f58d13b2dbdbc64a71bbb4324546a
cyril naimi <cyrilnaimi@gmail.com>
2025-08-01 12:09:54 +0200

    Add localization files and update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "poweron_gadget"),
+            name: "poweron_gadget",
+            resources: [.process("Resources")]),
     ]
 )
