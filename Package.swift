bf81455786e7b16bca40f22aba8bf9031824e755
cyril naimi <cyrilnaimi@gmail.com>
2025-08-01 14:18:56 +0200

    Add Info.plist and update Package.swift for .app bundle generation

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "poweron_gadget",
-            resources: [.process("Resources")]),
+            resources: [.process("Resources")],
+            infoPlist: .file("Info.plist")),
     ]
 )
